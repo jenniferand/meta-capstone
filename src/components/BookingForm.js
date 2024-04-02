a395b3820609df6b8b6258329e0171f3661b9be8
jenniferand <jennifer.anderson03@gmail.com>
2024-04-02 19:28:49 +0100

    make form components controlled

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,29 +1,71 @@
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from "@mui/x-date-pickers";
+import { useState } from 'react';
+import { validateEmail, validateDate, defineTime } from '../utils';
 
 function BookingForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState({ value: "", isTouched: false});
+  const [guests, setGuests] = useState(2);
+  const [datetime, setDatetime] = useState({ value: null, isTouched: false});
+  const [tablePref, setTablePref] = useState("Inside");
+  const [occasion, setOccasion] = useState("None");
+  const [comments, setComments] = useState("");
 
-  const minTime = new Date()
-  minTime.setHours(11)
-  minTime.setMinutes(0);
-  minTime.setSeconds(0);
-  minTime.setMilliseconds(0);
+  const getIsFormValid = () => {
+    return (
+      name &&
+      validateEmail(email.value) &&
+      validateDate(datetime.value) &&
+      guests
+    );
+  }
 
-  const maxTime = new Date()
-  maxTime.setHours(21)
-  maxTime.setMinutes(0);
-  maxTime.setSeconds(0);
-  maxTime.setMilliseconds(0);
+  const clearForm = () => {
+    setName("");
+    setEmail({ value: "", isTouched: false});
+    setGuests(2);
+    setDatetime({ value: null, isTouched: false});
+    setTablePref("Inside");
+    setOccasion("None");
+    setComments("");
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearForm();
+    console.log('Form submitted!')
+  }
+
+  const minTime = defineTime(11);
+
+  const maxTime = defineTime(21);
 
   return (
     <div className="booking-grid">
       <h1 className="page-header">Reserve a Table</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <label htmlFor="name">Full Name</label>
-        <input className="input" type="text" id="name" />
+        <input
+          className="input"
+          type="text"
+          id="name"
+          required={true}
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
         <label htmlFor="email">Email</label>
-        <input className="input" type="email" id="email" />
+        {email.isTouched && !validateEmail(email.value) ? (<p style={{color: "red"}}>Please enter a valid email address.</p>) : null}
+        <input
+          className="input"
+          type="email"
+          id="email"
+          required={true}
+          value={email.value}
+          onChange={e => setEmail({ ...email, value: e.target.value })}
+          onBlur={() => setEmail({ ...email, isTouched: true })}
+        />
         <label htmlFor="guests">Number of Guests</label>
         <input
           className="input"
@@ -32,8 +74,11 @@ function BookingForm() {
           min="1"
           max="10"
           id="guests"
+          value={guests}
+          onChange={e => setGuests(e.target.value)}
         />
         <label htmlFor="res-datetime">Date and Time</label>
+        {datetime.isTouched && !validateDate(datetime.value) ? (<p style={{color: "red"}}>The selected slot is in the past.</p>) : null}
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <DateTimePicker
             id="res-datetime"
@@ -46,19 +91,32 @@ function BookingForm() {
             timeSteps={{ minutes: 30 }}
             skipDisabled={true}
             sx={{ marginBottom: '1rem' }}
+            required={true}
+            value={datetime.value}
+            onChange={e => setDatetime({ ...datetime, value: e, isTouched: true})}
           />
         </LocalizationProvider>
         <div className="optional-group">
           <div className="input-container">
             <label htmlFor="table-pref">Table Preference</label>
-            <select className="input" id="table-pref">
+            <select
+              className="input"
+              id="table-pref"
+              value={tablePref}
+              onChange={e => setTablePref(e.target.value)}
+            >
               <option>Inside</option>
               <option>Outside</option>
             </select>
           </div>
           <div className="input-container">
             <label htmlFor="occasion">Occasion</label>
-            <select className="input" id="occasion">
+            <select
+              className="input"
+              id="occasion"
+              value={occasion}
+              onChange={e => setOccasion(e.target.value)}
+            >
               <option>None</option>
               <option>Birthday</option>
               <option>Anniversary</option>
@@ -68,8 +126,14 @@ function BookingForm() {
           </div>
         </div>
         <label htmlFor="comments">Additional Comments</label>
-        <textarea className="input" id="comments"></textarea>
-        <input className="button" id="button-large" type="submit" value="Confirm Reservation" />
+        <textarea
+          className="input"
+          id="comments"
+          placeholder="Special requests, dietary restrictions, etc."
+          value={comments}
+          onChange={e => setComments(e.target.value)}
+        />
+        <input className="button" id="button-large" type="submit" value="Confirm Reservation" disabled={!getIsFormValid()} />
       </form>
     </div>
   );
---
+++ src/utils.js
@@ -0,0 +1,21 @@
+export const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+export const validateDate = (date) => {
+    const now = new Date();
+    return date > now ? true : false;
+};
+
+export const defineTime = (hour) => {
+    const time = new Date();
+    time.setHours(hour);
+    time.setMinutes(0);
+    time.setSeconds(0);
+    time.setMilliseconds(0);
+    return time;
+};
